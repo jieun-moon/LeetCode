7b11047092bdf1a5181383cd97689f5da3f1d7dd
jieun-moon <84083487+jieun-moon@users.noreply.github.com>
2025-06-20 11:20:34 +0900

    Time: 308 ms (98.08%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT e.name
+FROM Employee e
+JOIN (
+    SELECT managerID
+    FROM Employee
+    WHERE managerID IS NOT NULL
+    GROUP BY managerID
+    HAVING count(*) >= 5
+    ) m
+ON e.id = m.managerID
